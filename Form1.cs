80aff4ac3d297adc1ea77e69f6400aeb53e52b5d
Prasad Prasanna <prasadkvithana@gmail.com>
2024-07-30 18:16:47 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteBook
+{
+    public partial class Form1 : Form
+    {
+        private string text;
+        private string name;
+        private string extention;
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+
+            KeyDown += new KeyEventHandler(shortCut);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            text = richTextBox1.Text;
+        }
+        
+        private void shortCut(object sender, KeyEventArgs e)
+        {
+            if(e.Control && e.KeyCode.Equals(Keys.S))
+            {
+                File.WriteAllText("note.txt", text);
+                MessageBox.Show("File Saved Successfully");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
